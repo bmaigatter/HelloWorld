ac77ff2ee974b02b1dfac1676de0623c33cf2c66
bmaigatter <45770426+bmaigatter@users.noreply.github.com>
2019-11-12 11:53:10 -0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 
-    
+  # Code taken from https://shitcode.net/worst/language/java
   public static MyClass {
       private static volatile ProcessManager singleton = null;
       
@@ -52,7 +52,41 @@ public class HelloWorld {
 
       return converterString.toString();
   }
-    
+  
+  public class MandateData {
+
+    public final UUID coreId;
+    public final UUID accountId;
+    public final String accountRef;
+    public final String creditorId;
+    public final String creditorName;
+    public final String debtorFirstName;
+    public final String debtorLastName;
+    public final String branchCode;
+    public final String accountNumber;
+    public final LocalDate signingDate;
+    public final Address debtorAddress;
+
+    private MandateData(UUID coreId, UUID accountId, String accountRef, String creditorId, String creditorName, String branchCode, String accountNumber, String debtorFirstName, String debtorLastName,
+                        LocalDate signingDate, Address debtorAddress) {
+        this.coreId = coreId;
+        this.accountId = accountId;
+        this.accountRef = accountRef;
+        this.creditorId = creditorId;
+        this.creditorName = creditorName;
+        this.debtorFirstName = debtorFirstName;
+        this.debtorLastName = debtorLastName;
+        this.branchCode = branchCode;
+        this.accountNumber = accountNumber;
+        this.signingDate = signingDate;
+        this.debtorAddress = debtorAddress;
+    }
+
+    public static MandateData creationMandateData(UUID coreId, UUID accountId, String accountRef, String creditorId, String creditorName, String branchCode, String accountNumber, String debtorFirstName, String debtorLastName,
+                                                  LocalDate signingDate, Address debtorAddress) {
+        return new MandateData(coreId, accountId, accountRef, creditorId, creditorName, branchCode, accountNumber, debtorFirstName, debtorLastName, signingDate, debtorAddress);
+    }  
+  
   public static void main(String[] args) {
       // Prints "Hello, World" to the terminal window.
       System.out.println("Hello, World");
