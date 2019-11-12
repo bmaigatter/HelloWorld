79fa7280c3f54d137d3a284edbc099dd12e83c42
bmaigatter <45770426+bmaigatter@users.noreply.github.com>
2019-11-12 11:47:35 -0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,60 @@
 public class HelloWorld {
 
-    public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
-    }
+    
+  public static MyClass {
+      private static volatile ProcessManager singleton = null;
+      
+      public static ProcessManager getInstance() throws Exception {
+          if (singleton == null) {
+              synchronized (MyClass.class) {
+                  if (singleton == null) {
+                      singleton = new ProcessManager();
+                  }
+              }
+          }
+          return singleton;
+      }
+  }
+
+  public void sleep(long duration) {
+      long start = System.currentTimeMillis();
+      while(true) {
+          if (System.currentTimeMillis() - start >= duration)
+              break;
+      }
+  }
+
+  public static returnTrue(boolean b){
+      if (b){
+          return true;
+      } else {
+          return true;
+      }
+  }
+    
+  private HashMap<String, Tuple6<float[], String[], String[], String[], String[], String>> memberNameChangedToProtectTheInnocent = null;
+
+  public String toString() {
+      List<Integer> accountLevels = new ArrayList<>();
+      List<String> accountIds = new ArrayList<>();
+
+      for (SelectedHierarchyLevel selectedHierarchyLevel : selectedHierarchyLevels) {
+          accountLevels.add(selectedHierarchyLevel.getLevel());
+          accountIds.add(selectedHierarchyLevel.getAccountRowId());
+      }
+
+      vetoAreValidIds(accountIds);
+      StringBuilder converterString = new StringBuilder();
+
+      convert(accountLevels, converterString);
+      converterString.append("$");
+      convert(accountIds, converterString);
+
+      return converterString.toString();
+  }
+    
+  public static void main(String[] args) {
+      // Prints "Hello, World" to the terminal window.
+      System.out.println("Hello, World");
+  }
 }
